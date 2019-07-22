799f7f4907ee3fe2b7c628f4242de45a68f78503
Angel <angelglezz.7@gmail.com>
2019-07-22 11:44:47 -0500

    fix heigth image directores, border main

--- js/indepth.js
+++ js/indepth.js
@@ -1,22 +1,34 @@
 $(document).ready(function(){
 	$(".show").fancybox();
     $(".show").attr('rel', 'lb_info').fancybox({padding: 0});
+    $(".show").fancybox({
+        'centerOnScroll': true
+    });
 
     $("#inicio-bar").click(function(){
     	$("#directores").hide();
     	$("#jornadas").hide();
     	$("#inicio").show();
+        $("#inicio-bar").css({"border-bottom": "2px solid #bb9d59"});
+        $("#directores-bar").css({"border-bottom": "none"});
+        $("#jornadas-bar").css({"border-bottom": "none"});
     });
 
 	$("#directores-bar").click(function(){
     	$("#directores").show();
     	$("#jornadas").hide();
     	$("#inicio").hide();
+        $("#inicio-bar").css({"border-bottom": "none"});
+        $("#directores-bar").css({"border-bottom": "2px solid #bb9d59"});
+        $("#jornadas-bar").css({"border-bottom": "none"});
     });
 
     $("#jornadas-bar").click(function(){
     	$("#directores").hide();
     	$("#jornadas").show();
     	$("#inicio").hide();
+        $("#inicio-bar").css({"border-bottom": "none"});
+        $("#directores-bar").css({"border-bottom": "none"});
+        $("#jornadas-bar").css({"border-bottom": "2px solid #bb9d59"});
     });
 });
